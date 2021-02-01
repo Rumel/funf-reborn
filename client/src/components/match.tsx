ce93c1537fe1c7f8288c1e1c0f806d4ac260cd7b
Dalton Dick <daltondick2010@gmail.com>
2021-02-01 12:56:32 -0600

    Get matches displaying

--- client/src/components/match.tsx
+++ client/src/components/match.tsx
@@ -1,6 +1,40 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
+import { LeagueEntry, Match as MatchType } from '../types';
 
-export const Match = () => {
-  return <Box>Match</Box>;
+type Props = {
+  away: LeagueEntry;
+  home: LeagueEntry;
+  match: MatchType;
+};
+
+export const Match = (props: Props) => {
+  const { away, home, match } = props;
+
+  return (
+    <Box border='1px solid black' margin='0.25rem'>
+      <Box padding='0.5rem'>
+        <Box display='flex'>
+          <Box flexGrow={1}>
+            <Text fontSize='xl' fontWeight='bold'>
+              {away.entry_name}
+            </Text>
+          </Box>
+          <Box float='right'>
+            <Text>{match.league_entry_1_points}</Text>
+          </Box>
+        </Box>
+        <Box display='flex'>
+          <Box flexGrow={1}>
+            <Text fontSize='xl' fontWeight='bold'>
+              {home.entry_name}
+            </Text>
+          </Box>
+          <Box float='right'>
+            <Text>{match.league_entry_2_points}</Text>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
--- client/src/components/matchContainer.tsx
+++ client/src/components/matchContainer.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid } from '@chakra-ui/react';
 
 import { Match } from './match';
+import { useStateContext } from '../store';
+import { useLeagueContext } from '../leagueStore';
+import _ from 'lodash';
 
 export const MatchContainer = () => {
+  const { state, dispatch } = useStateContext();
+  const { leagueState, leagueDispatch } = useLeagueContext();
+  const [selectedGameweek, setSelectedGameweek] = useState<number | null>(null);
+  const { game } = state;
+  const { league_entries, matches } = leagueState;
+
+  useEffect(() => {
+    if (game !== null) {
+      setSelectedGameweek(game.current_event);
+    }
+  }, [game]);
+
+  if (game === null || matches === null || selectedGameweek === null) {
+    return null;
+  }
+
+  const currentMatches = _.filter(matches, (m) => m.event === selectedGameweek);
+
   return (
     <Grid templateColumns='repeat(2, 1fr)'>
-      <Match></Match>
-      <Match></Match>
-      <Match></Match>
-      <Match></Match>
+      {currentMatches.map((m, i) => {
+        const away = _.find(league_entries, (le) => le.id === m.league_entry_1);
+        const home = _.find(league_entries, (le) => le.id === m.league_entry_2);
+
+        if (away && home) {
+          return <Match key={i} away={away} home={home} match={m}></Match>;
+        }
+
+        return null;
+      })}
     </Grid>
   );
 };
