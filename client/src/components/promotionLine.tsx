8b59fbcf6fc3ec377b4c0b8f765e8ab605592942
Dalton Dick <daltondick2010@gmail.com>
2021-02-09 13:00:51 -0600

    Make promotion line look a bit better

--- client/src/components/promotionLine.tsx
+++ client/src/components/promotionLine.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import { Box, Grid, GridItem, HStack } from '@chakra-ui/react';
+import { Flex, HStack } from '@chakra-ui/react';
 import { RELEGATION_COLORS } from '../constants';
 import { PromotionBox } from './promotionBox';
 
+const margin = '0rem 1rem';
+
 export const PromotionLine = () => {
   return (
-    <Grid>
-      <GridItem>
-        <Box float='right'>
-          <HStack spacing='0.5rem'>
-            <PromotionBox color={RELEGATION_COLORS.RELEGATION}></PromotionBox>
-            <p>Relegation</p>
-            <PromotionBox
-              color={RELEGATION_COLORS.POSSIBLE_RELEGATION}></PromotionBox>
-            <p>Relegation play-off</p>
-            <PromotionBox
-              color={RELEGATION_COLORS.POSSIBLE_PROMOTION}></PromotionBox>
-            <p>Promotion play-offs</p>
-            <PromotionBox color={RELEGATION_COLORS.PROMOTION}></PromotionBox>
-            <p>Promotion</p>
-          </HStack>
-        </Box>
-      </GridItem>
-    </Grid>
+    <Flex flexDir='row' flexWrap='wrap' justify='flex-end'>
+      <HStack margin={margin}>
+        <PromotionBox color={RELEGATION_COLORS.RELEGATION}></PromotionBox>
+        <p>Relegation</p>
+      </HStack>
+      <HStack margin={margin}>
+        <PromotionBox
+          color={RELEGATION_COLORS.POSSIBLE_RELEGATION}></PromotionBox>
+        <p>Relegation play-off</p>
+      </HStack>
+      <HStack margin={margin}>
+        <PromotionBox
+          color={RELEGATION_COLORS.POSSIBLE_PROMOTION}></PromotionBox>
+        <p>Promotion play-offs</p>
+      </HStack>
+      <HStack margin={margin}>
+        <PromotionBox color={RELEGATION_COLORS.PROMOTION}></PromotionBox>
+        <p>Promotion</p>
+      </HStack>
+    </Flex>
   );
 };
